486b25f87d4ed449095be64103c2759ea01fc3d3
NRG55 <maloj_1980@yahoo.fr>
2025-11-13 13:51:47 +0000

    Add message details page. Update index page and router.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,14 +1,17 @@
 const { Router } = require('express');
+const { v4: uuid } = require('uuid');
 
 const indexRouter = Router();
 
 const messages = [
     {
+        id: uuid(),
         text: "Hi there!",
         user: "Amando",
         added: new Date()
     },
     {
+        id: uuid(),
         text: "Hello World!",
         user: "Charles",
         added: new Date()
@@ -20,10 +23,17 @@ indexRouter.get("/", (req, res) => res.render("index", { title: "Mini Messageboa
 indexRouter.get("/new", (req, res) => res.render("form"));
 indexRouter.post("/new", (req, res) => {
     const { messageText, messageUser } = req.body;
-    
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
+
+    messages.push({ id: uuid(), text: messageText, user: messageUser, added: new Date() });
 
     res.redirect("/");
+});
+
+indexRouter.get("/message/:id", (req, res) => {
+    const messageId = req.params.id;
+    const message = messages.find((messageObject) => messageId === messageObject.id);
+
+    res.render("messageDetails", { message: message });
 });
 
 module.exports = indexRouter;
